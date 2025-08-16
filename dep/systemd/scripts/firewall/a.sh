22a4676fa7fef6ea4802969976ee55936f791291
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-16 12:27:24 -0300

    update

--- dep/systemd/scripts/firewall/a.sh
+++ dep/systemd/scripts/firewall/a.sh
@@ -113,8 +113,8 @@ host() {
 
     ssh() {
         # Filter Rules
-        nft add rule inet firelux input ip saddr 192.168.10.0 iif "vlan910" tcp dport 444 accept
-        nft add rule inet firelux output ip daddr 192.168.10.0 oif "vlan910" tcp sport 444 accept
+        nft add rule inet firelux input ip saddr 192.168.10.0 iif "gw266884" tcp dport 444 accept
+        nft add rule inet firelux output ip daddr 192.168.10.0 oif "gw266884" tcp sport 444 accept
     }
 
     # Call Child Functions
