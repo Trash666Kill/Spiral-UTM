a1505a0853bccb7f601dd8aa6f17859fd60156ae
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-19 12:29:12 -0300

    update

--- dep/temp.sh
+++ dep/temp.sh
@@ -1,8 +1,8 @@
 # Configure NAT and forwarding for Bridge (BR_TAP714)
 br_tap714() {
     # Masquerade Rules
-    nft add rule inet firelux postrouting ip saddr 10.0.11.0/24 oifname "$WAN" masquerade
+    nft add rule inet firelux postrouting ip saddr 10.0.11.0/24 oifname "$WAN0" masquerade
 
     # Forward Rules
-    nft add rule inet firelux forward iifname "br_tap714" oifname "$WAN" accept
+    nft add rule inet firelux forward iifname "br_tap714" oifname "$WAN0" accept
 }
