2ef6136b4d9040cb84a1f7438cc0638d16b31fda
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-20 13:07:23 -0300

    update

--- dep/systemd/scripts/firewall/temp.sh
+++ dep/systemd/scripts/firewall/temp.sh
@@ -10,3 +10,14 @@ chains() {
     nft add chain inet firelux postrouting { type nat hook postrouting priority srcnat \; policy accept \; }
     nft add rule inet firelux postrouting log prefix \"POSTROUTING: \" level info accept
 }
+
+
+
+
+chains() {
+    nft add chain inet firelux input { type filter hook input priority 0 \; policy drop \; }
+    nft add chain inet firelux output { type filter hook output priority 0 \; policy drop \; }
+    nft add chain inet firelux forward { type filter hook forward priority filter \; policy drop \; }
+    nft add chain inet firelux prerouting { type nat hook prerouting priority 0 \; policy accept \; }
+    nft add chain inet firelux postrouting { type nat hook postrouting priority srcnat \; policy accept \; }
+}
