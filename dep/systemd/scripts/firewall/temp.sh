041dabd99c87f1db6221d711e4edb636df36fa3a
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-21 09:21:22 -0300

    update

--- dep/systemd/scripts/firewall/temp.sh
+++ dep/systemd/scripts/firewall/temp.sh
@@ -25,6 +25,177 @@ chains() {
 nft add rule inet firelux forward iifname { "vlan966", "br_vlan966" } oifname "gw854807" accept
 
 
+
+{
+  "Dhcp4": {
+    // Configurações gerais do servidor DHCPv4
+    "interfaces-config": {
+      // Definição das interfaces de rede para operação do DHCP
+      "interfaces": ["gw099324", "vlan710", "vlan714", "vlan910", "br_vlan966"]
+    },
+    "lease-database": {
+      // Parâmetros do banco de dados para gerenciamento de leases
+      "type": "memfile",
+      "persist": true,
+      "name": "/var/lib/kea/kea-leases4.csv"
+    },
+    "subnet4": [
+      {
+        // Configurações da sub-rede 10.0.4.0/28
+        "id": 1,
+        "subnet": "10.0.4.0/28",
+        "interface": "gw099324",
+        "valid-lifetime": 43200,
+        "pools": [
+          {
+            // Faixa de endereços IP para alocação dinâmica
+            "pool": "10.0.4.1 - 10.0.4.13"
+          }
+        ],
+        "option-data": [
+          {
+            // Configuração do gateway padrão
+            "name": "routers",
+            "data": "10.0.4.14"
+          },
+          {
+            // Configuração dos servidores DNS
+            "name": "domain-name-servers",
+            "data": "10.0.6.1"
+          },
+          {
+            // Configuração do domínio padrão
+            "name": "domain-name",
+            "data": "pine.local.br"
+          },
+          {
+            // Configuração dos servidores NTP
+            "name": "ntp-servers",
+            "data": "10.0.6.1"
+          }
+        ]
+      },
+      {
+        // Configurações da sub-rede 172.16.10.0/24
+        "id": 2,
+        "subnet": "172.16.10.0/24",
+        "interface": "vlan710",
+        "valid-lifetime": 604800,
+        "pools": [
+          {
+            // Faixa de endereços IP para alocação dinâmica
+            "pool": "172.16.10.1 - 172.16.10.253"
+          }
+        ],
+        "option-data": [
+          {
+            // Configuração do gateway padrão
+            "name": "routers",
+            "data": "172.16.10.254"
+          },
+          {
+            // Configuração dos servidores DNS
+            "name": "domain-name-servers",
+            "data": "10.0.6.1"
+          },
+          {
+            // Configuração do domínio padrão
+            "name": "domain-name",
+            "data": "pine.local.br"
+          },
+          {
+            // Configuração dos servidores NTP
+            "name": "ntp-servers",
+            "data": "10.0.6.1"
+          }
+        ],
+        "reservations": [
+          {
+            // SRV59978
+            "hw-address": "ac:22:0b:2e:2b:f7",
+            "ip-address": "172.16.10.2",
+            "hostname": "srv59978"
+          }
+        ]
+      },
+      {
+        // Configurações da sub-rede 172.16.14.0/24
+        "id": 3,
+        "subnet": "172.16.14.0/24",
+        "interface": "vlan714",
+        "valid-lifetime": 604800,
+        "pools": [
+          {
+            // Faixa de endereços IP para alocação dinâmica
+            "pool": "172.16.14.1 - 172.16.14.253"
+          }
+        ],
+        "option-data": [
+          {
+            // Configuração do gateway padrão
+            "name": "routers",
+            "data": "172.16.14.254"
+          },
+          {
+            // Configuração dos servidores DNS
+            "name": "domain-name-servers",
+            "data": "10.0.6.1"
+          },
+          {
+            // Configuração do domínio padrão
+            "name": "domain-name",
+            "data": "pine.local.br"
+          },
+          {
+            // Configuração dos servidores NTP
+            "name": "ntp-servers",
+            "data": "10.0.6.1"
+          }
+        ]
+      },
+      {
+        // Configurações da sub-rede 192.168.10.0/24
+        "id": 4,
+        "subnet": "192.168.10.0/24",
+        "interface": "vlan910",
+        "valid-lifetime": 43200,
+        "pools": [
+          {
+            // Faixa de endereços IP para alocação dinâmica
+            "pool": "192.168.10.1 - 192.168.10.253"
+          }
+        ],
+        "option-data": [
+          {
+            // Configuração do gateway padrão
+            "name": "routers",
+            "data": "192.168.10.254"
+          },
+          {
+            // Configuração dos servidores DNS
+            "name": "domain-name-servers",
+            "data": "10.0.6.1"
+          },
+          {
+            // Configuração do domínio padrão
+            "name": "domain-name",
+            "data": "pine.local.br"
+          },
+          {
+            // Configuração dos servidores NTP
+            "name": "ntp-servers",
+            "data": "10.0.6.1"
+          }
+        ],
+        "reservations": [
+          {
+            // Reservas de endereços IP para dispositivos específicos
+            "hw-address": "dc:a2:66:91:73:95",
+            "ip-address": "192.168.10.26",
+            "hostname": "nb367095"
+          }
+        ]
+      },
       {
         // Configurações da sub-rede 192.168.66.0/24
         "id": 5,
@@ -60,3 +231,23 @@ nft add rule inet firelux forward iifname { "vlan966", "br_vlan966" } oifname "g
           }
         ]
       }
+    ],
+    "loggers": [
+      {
+        // Configurações de registro de logs do servidor DHCP
+        "name": "kea-dhcp4",
+        "output_options": [
+          {
+            // Definição do arquivo de saída para logs
+            "output": "/var/log/kea/kea-dhcp4.log"
+          }
+        ],
+        "severity": "INFO"
+      }
+    ]
+  }
+}
+
+
+
+
