9e513a6e1c77aa304c5416a3383adf3e5f46e132
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-23 11:40:06 -0300

    update

--- dep/systemd/scripts/firewall/a.sh
+++ dep/systemd/scripts/firewall/a.sh
@@ -57,17 +57,12 @@ chains() {
     nft add chain inet firelux forward { type filter hook forward priority filter \; policy drop \; }
     nft add chain inet firelux prerouting { type nat hook prerouting priority 0 \; policy accept \; }
     nft add chain inet firelux postrouting { type nat hook postrouting priority srcnat \; policy accept \; }
+}
 
-        info() {
-            nft add rule inet firelux input log prefix \"INPUT_DROP: \" level info drop
-            nft add rule inet firelux output log prefix \"OUTPUT_DROP: \" level info drop
-            nft add rule inet firelux forward log prefix \"FORWARD_DROP: \" level info drop
-            nft add rule inet firelux prerouting log prefix \"PREROUTING: \" level info accept
-            nft add rule inet firelux postrouting log prefix \"POSTROUTING: \" level info accept
-        }
-
-        # Call
-        info
+setup_logging() {
+    nft add rule inet firelux input log prefix \"INPUT_DROP: \" level info
+    nft add rule inet firelux output log prefix \"OUTPUT_DROP: \" level info
+    nft add rule inet firelux forward log prefix \"FORWARD_DROP: \" level info
 }
 
 # Allow established and related connections
@@ -151,6 +146,7 @@ main() {
     chains
     established_related
     host
+    setup_logging
     "
 
     for SERVICE in $SERVICES
