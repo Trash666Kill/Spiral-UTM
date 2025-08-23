0bc64bfd5da00d43e656849833567efab6ad9f85
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-23 11:41:54 -0300

    update

--- dep/systemd/scripts/firewall/temp.sh
+++ dep/systemd/scripts/firewall/temp.sh
@@ -1,7 +1,10 @@
+# LOGGING RULES - MOVIDA PARA CÁ
 info() {
     nft add rule inet firelux input log prefix \"INPUT_DROP: \" level info drop
     nft add rule inet firelux output log prefix \"OUTPUT_DROP: \" level info drop
     nft add rule inet firelux forward log prefix \"FORWARD_DROP: \" level info drop
-    nft add rule inet firelux prerouting log prefix \"PREROUTING: \" level info accept
-    nft add rule inet firelux postrouting log prefix \"POSTROUTING: \" level info accept
+    
+    # As regras de nat não precisam de "accept" aqui, a política da chain já é accept
+    # nft add rule inet firelux prerouting log prefix \"PREROUTING: \" level info accept
+    # nft add rule inet firelux postrouting log prefix \"POSTROUTING: \" level info accept
 }
