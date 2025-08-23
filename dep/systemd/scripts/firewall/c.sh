10bdb05a81e6a313fd92936f780df374c239b75a
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-23 19:24:10 -0300

    update

---
+++ dep/systemd/scripts/firewall/c.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# - The third stage of firewall rules, its criticality level is green.
+# It is intended for configuring custom firewall rules according to the environment's needs.
+# Be careful not to override default security rules defined in stages A and B.
