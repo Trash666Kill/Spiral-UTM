194aea64e23c633094f72418d434d3aab2266a6f
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-23 18:13:46 -0300

    update

--- inst.sh
+++ inst.sh
@@ -462,8 +462,7 @@ setup_firewall() {
     # Configure firewall services and scripts
     systemctl disable --now nftables --quiet
     cp -r systemd/scripts/firewall /root/.services/
-    chmod 700 /root/.services/firewall/*
-    chattr +i /root/.services/firewall/a.sh
+    chmod 700 /root/.services/firewall/* && chattr +i /root/.services/firewall/a.sh
 
     # --- Added Logging Logic ---
 
