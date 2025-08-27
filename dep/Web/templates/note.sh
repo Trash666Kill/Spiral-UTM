6fc007561fadd5b9263c76711895118baf746062
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-08-27 17:49:18 -0300

    update

--- dep/Web/templates/note.sh
+++ dep/Web/templates/note.sh
@@ -5,6 +5,6 @@ apt-get install python3 python3-pip python3-venv
 mkdir web-gui && cd web-gui
 python3 -m venv venv
 source venv/bin/activate
-pip3 install Flask Flask-Login
+pip3 install Flask Flask-Login Flask-Session
 
 python3 log_viewer.py
