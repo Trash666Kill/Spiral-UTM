342a038d6da9b4525c5ef4ccfc12599323865b9e
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-09-14 13:50:13 -0300

    update

--- dep/systemd/scripts/firewall/autossh.sh
+++ dep/systemd/scripts/firewall/autossh.sh
@@ -1,13 +1,57 @@
-autossh() {
-    local AUTOSSH_SCRIPT="/root/.services/autossh.sh"
-    # Filter Rules
-    nft add rule inet firelux output tcp dport 4635 accept
-    nft add rule inet firelux output tcp dport 4533 accept
-    nft add rule inet firelux output tcp dport 8096 accept
-    nft add rule inet firelux output tcp dport 9091 accept
-    nft add rule inet firelux output tcp dport 8080 accept
+#!/bin/bash
 
-    # Call
-    sleep 2
-    bash "$AUTOSSH_SCRIPT"
+# Close on any error (Optional)
+#set -e
+
+# Function to establish tunnel for port 4533
+tunnel_port_4533() {
+    local REMOTE_HOST="frlfryiwad7efpapvov5wnqzbf.com.br"
+    local REMOTE_PORT=4635
+    local REMOTE_USER="root"
+    local SERVER_ALIVE_INTERVAL=60
+    local LOCAL_IP="172.16.10.2"
+    local LOCAL_PORT=4533
+    local STRICT_HOST_KEY_CHECKING="true"
+
+    autossh -M 0 -f -N -T \
+        -R ${LOCAL_PORT}:${LOCAL_IP}:${LOCAL_PORT} \
+        -p ${REMOTE_PORT} \
+        ${REMOTE_USER}@${REMOTE_HOST} \
+        -o StrictHostKeyChecking=${STRICT_HOST_KEY_CHECKING} \
+        -o ServerAliveInterval=${SERVER_ALIVE_INTERVAL}
 }
+
+# Function to establish tunnel for port 8096
+tunnel_port_8096() {
+    local REMOTE_HOST="frlfryiwad7efpapvov5wnqzbf.com.br"
+    local REMOTE_PORT=4635
+    local REMOTE_USER="root"
+    local SERVER_ALIVE_INTERVAL=60
+    local LOCAL_IP="172.16.10.2"
+    local LOCAL_PORT=8096
+    local STRICT_HOST_KEY_CHECKING="true"
+
+    autossh -M 0 -f -N -T \
+        -R ${LOCAL_PORT}:${LOCAL_IP}:${LOCAL_PORT} \
+        -p ${REMOTE_PORT} \
+        ${REMOTE_USER}@${REMOTE_HOST} \
+        -o StrictHostKeyChecking=${STRICT_HOST_KEY_CHECKING} \
+        -o ServerAliveInterval=${SERVER_ALIVE_INTERVAL}
+}
+
+# Main function to orchestrate the tunnels
+main() {
+    TUNNELS="
+    tunnel_port_4533
+    tunnel_port_8096
+    "
+
+    for TUNNEL in $TUNNELS
+    do
+        $TUNNEL
+        sleep 4
+    done
+}
+
+# Execute main function
+main
--- dep/systemd/scripts/firewall/c.sh
+++ dep/systemd/scripts/firewall/c.sh
@@ -11,10 +11,25 @@ wireguard() {
 
 }
 
+autossh() {
+    local AUTOSSH_SCRIPT="/root/.services/firewall/autossh.sh"
+    # Filter Rules
+    nft add rule inet firelux output tcp dport 4635 accept
+    nft add rule inet firelux output tcp dport 4533 accept
+    nft add rule inet firelux output tcp dport 8096 accept
+    nft add rule inet firelux output tcp dport 9091 accept
+    nft add rule inet firelux output tcp dport 8080 accept
+
+    # Call
+    sleep 2
+    bash "$AUTOSSH_SCRIPT"
+}
+
 # Main function to orchestrate the setup
 main() {
     RULES="
     wireguard
+    autossh
     "
 
     for RULE in $RULES
