95358c1d0a6e75aa8c2cf1619686c56027134fb9
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2025-11-01 22:49:12 -0300

    update

--- dep/temp.sh
+++ dep/temp.sh
@@ -1,41 +1,130 @@
-setup_firewall() {
-    log_info "Starting Firewall Setup"
-    
-    # Install required dependencies
-    run_apt install nftables rsyslog
+# Configure NAT and forwarding for Server (VLAN710)
+server() {
+    # Masquerade Rules
+    nft add rule inet firelux postrouting ip saddr 172.16.10.0/24 oifname "$WAN0" masquerade
 
-    # Configure firewall services and scripts
-    systemctl disable --now nftables --quiet
-    cp -r systemd/scripts/firewall /root/.services/
-    chmod 700 /root/.services/firewall/*
-    chattr +i /root/.services/firewall/a.sh
+    # SNAT Rules
+    nft add rule inet firelux postrouting oif "vlan710" ip saddr 172.16.10.0/24 snat to 172.16.10.254
 
-    # --- Added Logging Logic ---
+    # Forward Rules
+    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol icmp accept
+    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol udp udp dport 53 accept
+    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol tcp tcp dport 53 accept
+    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol tcp tcp dport {80, 443} accept
 
-    log_info "Configuring rsyslog for nftables logging..."
-    # Create the rsyslog configuration file to filter nftables logs
-    cat <<EOF > /etc/rsyslog.d/50-nftables.conf
-# /etc/rsyslog.d/50-nftables.conf
-:msg, contains, "INPUT_DROP: " /var/log/nftables.log
-:msg, contains, "OUTPUT_DROP: " /var/log/nftables.log
-:msg, contains, "FORWARD_DROP: " /var/log/nftables.log
-& stop
-EOF
+    # Child Functions
+    srv28013_445() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 445 dnat to 172.16.10.1:445
 
-    log_info "Setting up log rotation for nftables..."
-    # Create the configuration file for nftables log rotation
-    cat <<'EOF' > /etc/logrotate.d/nftables
-/var/log/nftables.log
-{
-    rotate 7
-    daily
-    missingok
-    notifempty
-    delaycompress
-    compress
-    postrotate
-        systemctl restart rsyslog > /dev/null
-    endscript
-}
-EOF
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 445 accept
+        nft add rule inet firelux forward iif "vlan710" oif "vlan910" tcp sport 445 accept
+    }
+
+    srv28013_4242() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 4242 dnat to 172.16.10.1:4242
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 4242 accept
+    }
+
+    srv28013_6600() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6600 dnat to 172.16.10.1:6600
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6600 accept
+    }
+
+    srv28013_5644() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 5644 dnat to 172.16.10.1:5644
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 5644 accept
+    }
+
+    srv28013_4533() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 4533 dnat to 172.16.10.1:4533
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 4533 accept
+    }
+
+    srv28013_8096() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 8096 dnat to 172.16.10.1:8096
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 8096 accept
+    }
+
+    srv28013_6080() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6080 dnat to 172.16.10.1:6080
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6080 accept
+    }
+
+    srv28013_6081() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6081 dnat to 172.16.10.1:6081
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6081 accept
+    }
+
+    srv28013_8081() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 8081 dnat to 172.16.10.1:8081
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 8081 accept
+    }
+
+    srv28013_3389() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 3389 dnat to 172.16.10.1:3389
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 3389 accept
+    }
+
+    srv28013_vnc() {
+        # DNAT Rules
+        for port in $(seq 5900 5960); do
+            nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport $port dnat to 172.16.10.1:$port
+        done
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 5900-5960 accept
+        nft add rule inet firelux forward iif "vlan710" oif "vlan910" tcp sport 5900-5960 accept
+    }
+
+    inter_vlan_4242() {
+        # DNAT Rules
+        nft add rule inet firelux prerouting iif "vlan714" ip daddr 172.16.14.0/24 tcp dport 4242 dnat to 172.16.10.0:4242
+
+        # Forward Rules
+        nft add rule inet firelux forward iif "vlan714" oif "vlan710" tcp dport 4242 accept
+        nft add rule inet firelux forward iif "vlan710" oif "vlan714" tcp sport 4242 accept
+    }
+
+    # Call Child Functions
+    srv28013_445
+    srv28013_4242
+    srv28013_5644
+    srv28013_6600
+    srv28013_4533
+    srv28013_8096
+    srv28013_6080
+    srv28013_6081
+    srv28013_8081
+    srv28013_3389
+    srv28013_vnc
+    inter_vlan_4242
 }
