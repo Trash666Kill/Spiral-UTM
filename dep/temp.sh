51cab30e85cb02b5fb26f0b032e2ded0a6871b7f
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2026-01-05 22:40:01 -0300

    update

--- dep/temp.sh
+++ dep/temp.sh
@@ -1,130 +1,169 @@
-# Configure NAT and forwarding for Server (VLAN710)
-server() {
-    # Masquerade Rules
-    nft add rule inet firelux postrouting ip saddr 172.16.10.0/24 oifname "$WAN0" masquerade
+#!/bin/bash
 
-    # SNAT Rules
-    nft add rule inet firelux postrouting oif "vlan710" ip saddr 172.16.10.0/24 snat to 172.16.10.254
+# Close on any error
+set -e
 
-    # Forward Rules
-    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol icmp accept
-    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol udp udp dport 53 accept
-    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol tcp tcp dport 53 accept
-    nft add rule inet firelux forward iifname "vlan710" oifname "$WAN0" ip protocol tcp tcp dport {80, 443} accept
+# Enable IP forwarding
+ip_forwarding() {
+    sysctl -w net.ipv4.ip_forward=1
+    if [[ $? -ne 0 ]]; then
+        printf "\e[31m*\e[0m Error: Failed to enable IP forwarding.\n"
+        exit 1
+    fi
+}
 
-    # Child Functions
-    srv28013_445() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 445 dnat to 172.16.10.1:445
+# Restart nftables service
+restart_nftables() {
+    local SERVICE=nftables
+    echo "Restarting $SERVICE..."
+    systemctl restart "$SERVICE"
+    if [[ $? -ne 0 ]]; then
+        printf "\e[31m*\e[0m Error: Failed to restart $SERVICE.\n"
+        exit 1
+    fi
+}
 
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 445 accept
-        nft add rule inet firelux forward iif "vlan710" oif "vlan910" tcp sport 445 accept
+# Flush existing nftables rules
+flush_nftables() {
+    echo "Flushing ruleset..."
+    nft flush ruleset
+}
+
+# Create main table
+main_table() {
+    echo "Creating table..."
+    nft add table inet firelux
+}
+
+# Main function for the blacklist system
+blacklist_set() {
+    echo "Configuring Blacklist..."
+    # Cria o set
+    nft add set inet firelux blacklist { type ipv4_addr \; flags interval \; }
+
+    blacklist_elements() {
+        echo "Adding elements to blacklist..."
+        nft add element inet firelux blacklist { 45.231.112.0/22 }
     }
 
-    srv28013_4242() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 4242 dnat to 172.16.10.1:4242
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 4242 accept
-    }
-
-    srv28013_6600() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6600 dnat to 172.16.10.1:6600
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6600 accept
-    }
-
-    srv28013_5644() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 5644 dnat to 172.16.10.1:5644
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 5644 accept
-    }
-
-    srv28013_4533() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 4533 dnat to 172.16.10.1:4533
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 4533 accept
-    }
-
-    srv28013_8096() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 8096 dnat to 172.16.10.1:8096
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 8096 accept
-    }
-
-    srv28013_6080() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6080 dnat to 172.16.10.1:6080
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6080 accept
-    }
-
-    srv28013_6081() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 6081 dnat to 172.16.10.1:6081
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 6081 accept
-    }
-
-    srv28013_8081() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 8081 dnat to 172.16.10.1:8081
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 8081 accept
-    }
-
-    srv28013_3389() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport 3389 dnat to 172.16.10.1:3389
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 3389 accept
-    }
-
-    srv28013_vnc() {
-        # DNAT Rules
-        for port in $(seq 5900 5960); do
-            nft add rule inet firelux prerouting iif "vlan910" ip daddr 192.168.10.0/24 tcp dport $port dnat to 172.16.10.1:$port
-        done
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan910" oif "vlan710" tcp dport 5900-5960 accept
-        nft add rule inet firelux forward iif "vlan710" oif "vlan910" tcp sport 5900-5960 accept
-    }
-
-    inter_vlan_4242() {
-        # DNAT Rules
-        nft add rule inet firelux prerouting iif "vlan714" ip daddr 172.16.14.0/24 tcp dport 4242 dnat to 172.16.10.0:4242
-
-        # Forward Rules
-        nft add rule inet firelux forward iif "vlan714" oif "vlan710" tcp dport 4242 accept
-        nft add rule inet firelux forward iif "vlan710" oif "vlan714" tcp sport 4242 accept
+    blacklist_rules() {
+        echo "Applying blacklist rules..."
+        nft add rule inet firelux input ip saddr @blacklist drop
+        nft add rule inet firelux forward ip saddr @blacklist drop
     }
 
     # Call Child Functions
-    srv28013_445
-    srv28013_4242
-    srv28013_5644
-    srv28013_6600
-    srv28013_4533
-    srv28013_8096
-    srv28013_6080
-    srv28013_6081
-    srv28013_8081
-    srv28013_3389
-    srv28013_vnc
-    inter_vlan_4242
+    blacklist_elements
+    blacklist_rules
 }
+
+# Create chains with default drop policy
+chains() {
+    echo "Creating chains..."
+    nft add chain inet firelux input { type filter hook input priority 0 \; policy drop \; }
+    nft add chain inet firelux output { type filter hook output priority 0 \; policy drop \; }
+    nft add chain inet firelux forward { type filter hook forward priority filter \; policy drop \; }
+    nft add chain inet firelux prerouting { type nat hook prerouting priority 0 \; policy accept \; }
+    nft add chain inet firelux postrouting { type nat hook postrouting priority srcnat \; policy accept \; }
+}
+
+# Setup logging for dropped packets
+setup_logging() {
+    echo "Setting up logging..."
+    nft add rule inet firelux input log prefix \"INPUT_DROP: \" level info
+    nft add rule inet firelux output log prefix \"OUTPUT_DROP: \" level info
+    nft add rule inet firelux forward log prefix \"FORWARD_DROP: \" level info
+}
+
+# Allow established and related connections (essential for stateful firewall)
+established_related() {
+    echo "Allowing established/related connections..."
+    nft add rule inet firelux input ct state established,related accept
+    nft add rule inet firelux output ct state established,related accept
+    nft add rule inet firelux forward ct state established,related accept
+}
+
+# Configure host-specific rules
+host() {
+    echo "Configuring host rules..."
+    # Filter Rules
+    loopback() {
+        nft add rule inet firelux input iif "lo" accept
+        nft add rule inet firelux output oif "lo" accept
+    }
+
+    # Filter Rules
+    icmp() {
+        nft add rule inet firelux input icmp type { echo-request, echo-reply, destination-unreachable, time-exceeded } accept
+        nft add rule inet firelux output icmp type { echo-request, echo-reply, destination-unreachable, time-exceeded } accept
+    }
+
+    # Filter Rules
+    dns() {
+        nft add rule inet firelux output udp dport 53 accept
+        nft add rule inet firelux output tcp dport 53 accept
+    }
+
+    # Filter Rules
+    ntp() {
+        nft add rule inet firelux output udp dport 123 accept
+    }
+
+    # Filter Rules
+    web() {
+        nft add rule inet firelux output tcp dport 80 accept
+        nft add rule inet firelux output tcp dport 443 accept
+        # SSH do Host
+        nft add rule inet firelux input tcp dport { 4634, 4635 } accept
+    }
+
+    # Call Child Functions
+    loopback
+    icmp
+    dns
+    ntp
+    web
+}
+
+# Função para as regras de NAT e Forwarding das VMs (necessário para manter seu ambiente funcional)
+vms_and_nat() {
+    echo "Configuring VM Forwarding and NAT..."
+    # DNAT
+    nft add rule inet firelux prerouting iifname "eth0" tcp dport 80 dnat ip to 10.0.11.3:80
+    nft add rule inet firelux prerouting iifname "eth0" tcp dport 443 dnat ip to 10.0.11.3:443
+    nft add rule inet firelux prerouting iifname "eth0" tcp dport 7881 dnat ip to 10.0.11.9:7881
+    nft add rule inet firelux prerouting iifname "eth0" udp dport 50000-60000 dnat ip to 10.0.11.9
+    
+    # Forwarding Accept
+    nft add rule inet firelux forward ip daddr 10.0.11.3 tcp dport { 80, 443 } accept
+    nft add rule inet firelux forward ip daddr 10.0.11.9 tcp dport 7881 accept
+    nft add rule inet firelux forward ip daddr 10.0.11.9 udp dport 50000-60000 accept
+    nft add rule inet firelux forward iifname "br_tap111" oifname "eth0" accept
+    
+    # Masquerade
+    nft add rule inet firelux postrouting ip saddr 10.0.11.0/24 oifname "eth0" masquerade
+}
+
+# Main function to orchestrate the setup
+main() {
+    RULES="
+    ip_forwarding
+    restart_nftables
+    flush_nftables
+    main_table
+    chains
+    blacklist_set
+    established_related
+    host
+    vms_and_nat
+    setup_logging
+    "
+
+    for RULE in $RULES
+    do
+        $RULE
+    done
+}
+
+# Execute main function
+main
